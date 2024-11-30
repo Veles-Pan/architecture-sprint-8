96e47d9fdc387c5afc6269234fe335829fd38e18
Viktory <73409754+Veles-Pan@users.noreply.github.com>
2024-11-30 17:24:50 +0000

    feat: tasks

---
+++ api/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ReportsModule } from './reports/reports.module';
+
+@Module({
+  imports: [AuthModule, ReportsModule],
+})
+export class AppModule {}
